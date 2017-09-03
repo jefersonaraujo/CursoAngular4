8514b5b109401ef388c14e4d612ed1c25edfbd83
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-09-03 15:19:03 -0300

    Add component

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule
