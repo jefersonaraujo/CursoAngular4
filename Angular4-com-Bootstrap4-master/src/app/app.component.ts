e576a3571c7b52d2bf675b86eae2c90b7f2b8ab0
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-04-04 10:21:49 -0300

    Add firebase

--- Angular4-com-Bootstrap4-master/src/app/app.component.ts
+++ Angular4-com-Bootstrap4-master/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+//Firebase
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  constructor(public afAuth: AngularFireAuth){
+  this.user= this.afAuth.authState;
+}
+
+loginFacebook(){
+  this.afAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider());
+}
 }
