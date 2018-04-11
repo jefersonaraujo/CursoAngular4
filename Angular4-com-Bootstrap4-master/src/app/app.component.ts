b26f78162e959bebab193a55ae4a3c859aa922d6
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-04-11 15:20:32 -0300

    vinculando email

--- Angular4-com-Bootstrap4-master/src/app/app.component.ts
+++ Angular4-com-Bootstrap4-master/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-//Firebase
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -12,20 +11,20 @@ import * as firebase from 'firebase/app';
 })
 export class AppComponent {
   user: Observable<firebase.User>;
+  nome:string;
   email:string;
   senha:string;
-  nome:string;
   urlImagem:string;
 
   constructor(public afAuth: AngularFireAuth){
-  this.user= this.afAuth.authState;
+    this.user= this.afAuth.authState;
   }
 
-loginFacebook(){
-  this.afAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider());
-}
+  loginFacebook(){
+    this.afAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider());
+  }
 
-vinculaFacebook(){
+  vinculaFacebook(){
     this.afAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider()).then((res:any)=>{
       firebase.auth().currentUser.linkWithCredential(res.credential).then((user)=>{
         console.log('OK');
@@ -41,63 +40,62 @@ vinculaFacebook(){
     });
   }
 
+  loginGithub(){
+    this.afAuth.auth.signInWithPopup( new firebase.auth.GithubAuthProvider());
+  }
 
-
-loginEmail(){
-  firebase.auth().signInWithEmailAndPassword(this.email, this.senha).catch((erro:any)=>{
-    console.log(erro);
-
-  });
-}
-
-cadastroEmail(){
-  //console.log(this.email,this.senha);
-  firebase.auth().createUserWithEmailAndPassword(this.email,this.senha).then((res:any)=>{
-    console.log(res);
-    let usuario = firebase.auth().currentUser;
-    usuario.updateProfile({
-      displayName:this.nome,
-      photoURL:this.urlImagem
+  vinculaGithub(){
+    this.afAuth.auth.signInWithPopup( new firebase.auth.GithubAuthProvider()).then((res:any)=>{
+      firebase.auth().currentUser.linkWithCredential(res.credential).then((user)=>{
+        console.log('OK');
+      },(erro)=>{
+        console.log('ERRO');
+      });
+    }).catch((erro:any)=>{
+      firebase.auth().currentUser.linkWithCredential(erro.credential).then((user)=>{
+        console.log('OK');
+      },(erro)=>{
+        console.log('ERRO');
+      });
     });
+  }
 
-  }).catch((erro:any)=>{
-    console.log(erro);
-  });
-}
+  loginEmail(){
+    //console.log(this.email,this.senha);
+    firebase.auth().signInWithEmailAndPassword(this.email,this.senha).catch((erro:any)=>{
+      console.log(erro);
+    });
+  }
 
-vinculaEmail(){
-  let credential = firebase.auth.EmailAuthProvider.credential(this.email,this.senha);
-  firebase.auth().currentUser.linkWithCredential(credential).then((user)=>{
-    console.log('OK');
-  },(erro)=>{
-    console.log(erro);
-    console.log('ERRO');
-  });
-}
-
-
-loginGithub(){
-  this.afAuth.auth.signInWithPopup( new firebase.auth.GithubAuthProvider());
-}
-
-vinculaGithub(){
-  this.afAuth.auth.signInWithPopup( new firebase.auth.GithubAuthProvider()).then((res:any)=>{
-    firebase.auth().currentUser.linkWithCredential(res.credential).then((user)=>{
+  vinculaEmail(){
+    let credential = firebase.auth.EmailAuthProvider.credential(this.email,this.senha);
+    firebase.auth().currentUser.linkWithCredential(credential).then((user)=>{
       console.log('OK');
     },(erro)=>{
+      console.log(erro);
       console.log('ERRO');
     });
-  }).catch((erro:any)=>{
-    firebase.auth().currentUser.linkWithCredential(erro.credential).then((user)=>{
-      console.log('OK');
-    },(erro)=>{
-      console.log('ERRO');
+  }
+
+
+  cadastroEmail(){
+    //console.log(this.email,this.senha);
+    firebase.auth().createUserWithEmailAndPassword(this.email,this.senha).then((res:any)=>{
+      console.log(res);
+      let usuario = firebase.auth().currentUser;
+      usuario.updateProfile({
+        displayName:this.nome,
+        photoURL:this.urlImagem
+      });
+
+    }).catch((erro:any)=>{
+      console.log(erro);
     });
-  });
-}
+  }
 
-sair(){
-  this.afAuth.auth.signOut();
+  sair(){
+    this.afAuth.auth.signOut();
+  }
 
-}
+
 }
